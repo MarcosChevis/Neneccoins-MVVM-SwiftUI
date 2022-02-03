92b5bae65bab25f4c6e03ede3c9c2cf5ceadbf8c
Marcos Chevis <marcoschevism@gmail.com>
2022-02-03 16:58:36 -0300

    Initial Commit

---
+++ Neneccoins-MVVM-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neneccoins-MVVM-SwiftUI
+//
+//  Created by Marcos Chevis on 03/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neneccoins-MVVM-SwiftUI/Neneccoins_MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neneccoins_MVVM_SwiftUIApp.swift
+//  Neneccoins-MVVM-SwiftUI
+//
+//  Created by Marcos Chevis on 03/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Neneccoins_MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
